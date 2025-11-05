4b5971d4f0eb0ba86ff980dd56aa91a86512f6ec
sotu02S <ghoshsoutam9@gmail.com>
2025-11-06 00:16:24 +0530

    Fix photo URL for the found item

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ function initSampleData() {
             poster: 'John Doe',
             status: 'unclaimed',
             date: new Date().toLocaleDateString(),
-            photo: 'https://images.unsplash.com/photo-1602143407151-7111542de6e8?w=400',
+            photo: 'https://miro.medium.com/1*n35mA_P-qf8lahgwaIzEOw.jpeg',
             messages: []
         },
         {
@@ -175,3 +175,4 @@ window.openMessages = openMessages;
 window.markClaimed = markClaimed;
 window.closeModal = closeModal;
 window.showTab = showTab;
+
